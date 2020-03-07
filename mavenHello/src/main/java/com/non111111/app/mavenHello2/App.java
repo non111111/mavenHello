f5186fc573adb16aab7847eed56311d0cc301721
non111111 <non_111111@hotmail.com>
2020-03-07 11:10:19 +1100

    1st

---
+++ mavenHello/src/main/java/com/non111111/app/mavenHello2/App.java
@@ -0,0 +1,13 @@
+package com.non111111.app.mavenHello2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
